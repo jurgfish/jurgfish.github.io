4cb747f6802c91ea6f73c071a061ee9c1aa1de62
jurgfish <jurgfish@gmail.com>
2021-02-04 22:22:57 -0600

    restyle and simplify

--- anim.js
+++ anim.js
@@ -1,9 +1,9 @@
 /**
- *
+ * 
  *
  */
 
-
+// reset position when refreshing page
 if ('scrollRestoration' in history) {
     history.scrollRestoration = 'manual';
 }
@@ -11,138 +11,91 @@ document.body.scrollTop = 0;
 document.documentElement.scrollTop = 0;
 
 var body = document.getElementsByTagName("body")[0];
-var elems = body.getElementsByClassName("anim");   
+var elems = body.getElementsByClassName("anim");
+var elemIdx = 0;
+var logoPos = 10;
 var typespeed = 20;
+var titleTypespeed = 50;
+var titleTimeout = 2000;
+var animTimeout = 500;
+var entryTimeout = 100;
 
 animateLogo()
 
 function animateLogo() {
-    var im = document.getElementById("logo");
-    var mt = -100;
+    var elem = document.getElementById("logo");
+    var marginTop = -100;
     var slider = setInterval(frame, 10);
 
     function frame() {
-        if (mt == 5) {
+        if (marginTop == logoPos) {
             clearInterval(slider);
             return;
-        }
-        if (mt == -90) {
-            im.style.visibility = "visible";
-        }
-        if (mt != 5) {
-            mt++; 
-            im.style.marginTop = mt + "%"; 
+        } else if (marginTop == -90) {
+            elem.style.visibility = "visible";
+        } if (marginTop != logoPos) {
+            marginTop++; 
+            elem.style.marginTop = marginTop + "%"; 
         }
     }
 
-    setTimeout("wipeheaderin()", 2000);
+    setTimeout("wipeheaderin()", titleTimeout);
 }
 
-// function wipeText(elem, wipe_right, callback) {
-
-//     var wiper = setInterval(frame, 50);
-
-//     var word = elem.textContent;
-//     var c = wipe_right ? 0 : word.length;
-
-//     function frame() {
-//         elem.innerHTML = word.substring(0, c);
-
-//         if (wipe_right) {
-//             c++;
-//             if (c == 1) {
-//                 elem.style.visibility = "visible";
-//             } else if(c == word.length) {
-//                 clearInterval(wiper);
-//             }
-//         } else {
-//             c--;
-//             if (c == 0) {
-//                 clearInterval(wiper);
-//                 elem.style.visibility = "hidden";
-//                 elem.innerHTML = word;
-//             }
-//         }
-//     }
-
-//     setTimeout(function () { callback(); }, 500);
-// }
-
 function wipeheaderin() {
-
-    var wiper = setInterval(frame, 50);
-
-    var elem = document.getElementById("jurgfish");
-
+    var elem = document.getElementById("title");
     var word = elem.textContent;
     var c = 0;
-    // var c = 0; wipe_right ? 0 : words.length;
+    var wiper = setInterval(frame, titleTypespeed);
 
     function frame() {
         elem.innerHTML = word.substring(0, c);
-
-        // if (wipe_right) {
         c++;
         if (c == 1) {
             elem.style.visibility = "visible";
-        } else if(c == word.length) {
+        } else if (c == word.length) {
             clearInterval(wiper);
         }
-        // } else {
-        //     c--;
-        //     if (c == 0) {
-        //         clearInterval(wiper);
-        //         elem.style.visibility = "hidden";
-        //         elem.innerHTML = word;
-        //     }
-        // }
     }
 
-    setTimeout("animateEntries()", 500);
-    // setTimeout(function () { callback(); }, 500);
-    // wipeText(document.getElementById("jurgfish"), true, animateEntries);
+    slideUp(document.getElementsByClassName("links")[0]);
+    setTimeout("animateEntries()", animTimeout);
+} 
+
+function animateEntries() {
+    setTimeout(function () {
+        var elem = elems[elemIdx];
+        var bound = elem.getBoundingClientRect();
+        if (bound.top < (window.innerHeight * 0.80 || 
+                document.documentElement.clientHeight * 0.80)) {
+            elem = elems[elemIdx]
+            slideUp(elem);
+            typeWords(elem);
+            elemIdx++;
+        } if (elemIdx < elems.length) {
+            animateEntries();
+        }
+    }, entryTimeout)
 }
 
-var j = 0;            
-
-function animateEntries() {           
-   setTimeout(function () {    
-       var rect = elems[j].getBoundingClientRect();
-       if (rect.top < (window.innerHeight * 0.80 || 
-               document.documentElement.clientHeight * 0.80)) {
-           slideUp(j);
-           j++;
-       }
-       if (j < elems.length) {
-           animateEntries();
-       }
-   }, 100)
-}
-
-function slideUp(j) {
-    var elem = elems[j];
-    var mt = 100;
+function slideUp(elem) {
+    var marginTop = 100;
     var slider = setInterval(frame, 1);
 
     function frame() {
-        if (mt == 0) {
+        if (marginTop == 0) {
             clearInterval(slider);
             return;
-        } else if (mt == 90) {
+        } else if (marginTop == 90) {
             elem.style.visibility = "visible";
-        }
-        if (mt != 0) {
-            mt -= 1;
-            elem.style.marginTop = mt + "%"; 
+        } if (marginTop != 0) {
+            marginTop -= 1;
+            elem.style.marginTop = marginTop + "%"; 
         }
     }
-
-    typeWords(j);
 }
 
-function typeWords(j) {
-    var elem = elems[j];
-
+function typeWords(elem) {
     var fullText = elem.textContent;
     var wordSet = fullText.split(" ");
     var currentOffset = 0;
@@ -164,4 +117,10 @@ function typeWords(j) {
         elem.innerHTML = text;
     }
 }
+
+
+
+
+
+
 
