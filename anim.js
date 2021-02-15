b470b0f65fa16f2a5c76e5517b9f12097fad0ed9
jurgfish <jurgfish@gmail.com>
2021-02-14 18:05:44 -0600

    update test entries

--- anim.js
+++ anim.js
@@ -2,7 +2,7 @@
 
 ////////////////////////////////////////////////////////////////////////////
 
-var version = "j099."
+var version = "j100."
 
 // text elements
 var logoElem = document.getElementById("logo");
