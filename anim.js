3db15df9df7a63c319bcd858d18d1bb9459e78bd
jurgfish <jurgfish@gmail.com>
2021-02-16 23:41:58 -0600

    reposition top button

--- anim.js
+++ anim.js
@@ -2,7 +2,7 @@
 
 ////////////////////////////////////////////////////////////////////////////
 
-var version = "j131."
+var version = "j132."
 
 // text elements
 var logoElem = document.getElementById("logo");
