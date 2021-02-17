e25ab67f7b03bf3c50c34d8553f9d13997321c9f
jurgfish <jurgfish@gmail.com>
2021-02-16 18:02:01 -0600

    update button pos

--- anim.js
+++ anim.js
@@ -2,7 +2,7 @@
 
 ////////////////////////////////////////////////////////////////////////////
 
-var version = "j120."
+var version = "j121."
 
 // text elements
 var logoElem = document.getElementById("logo");
