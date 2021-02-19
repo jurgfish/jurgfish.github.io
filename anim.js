26823922d13ec826ac1f02bf8a3a87799c6d9919
jurgfish <jurgfish@gmail.com>
2021-02-18 22:50:34 -0600

    title links

--- anim.js
+++ anim.js
@@ -4,7 +4,7 @@
 
 ////////////////////////////////////////////////////////////////////////////
 
-var version = "j148."
+var version = "j149."
 
 // elements
 var logoElem = document.getElementById("logo");
